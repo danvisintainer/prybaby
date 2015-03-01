915fb432c26dce5a0d499c5b7789546de212f803
danvisintainer <dan@danvisintainer.com>
2015-03-01 15:15:09 -0500

    add argument error checking

--- lib/prybaby.rb
+++ lib/prybaby.rb
@@ -1,18 +1,43 @@
 class Prybaby
 
   class << self
-    attr_accessor :lines_modified, :files_modified
+    attr_accessor :mode, :lines_modified, :files_modified
   end
 
   def self.go(args)
+    binding.pry
+
+    Prybaby.mode = ''
     Prybaby.lines_modified = 0
     Prybaby.files_modified = 0
-    load_files
+
+    process_args(args)
+
+    if self.mode == 'h' || 'e'
+      puts "Sorry, I didn't recognize those arguments." if self.mode == 'e'
+      print_help
+    else
+      load_files
+    end
+
   end
 
-  def self.load_files
-    puts "Loading files..."
+  def self.print_help
+    puts 'Help goes here!'
+  end
 
+  def self.process_args(args)
+    args = args.gsub('-', '')
+    self.mode = 'h' if args == 'help'
+
+    if args.length > 1 || args.scan(/u|r|c/).empty?
+      self.mode = 'e'
+    else
+      self.mode = args
+    end
+  end
+
+  def self.load_files()
     Dir.glob(File.join(".", "**", "*.rb")).each do |file|
       remove_breakpoints(file)
     end
@@ -36,6 +61,10 @@ class Prybaby
     temp_file.close
     FileUtils.mv(temp_file.path, file)
     self.files_modified += 1 if file_changed
+  end
+
+  def self.comment_out_breakpoints(file)
+
   end
 
   def self.print_removed_breakpoint_stats
