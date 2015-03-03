be1cde67f81ce7fa0ae3bc81daa6d7fcf2612f5f
danvisintainer <dan@danvisintainer.com>
2015-03-03 09:41:02 -0500

    better tempfile handling and better comments
    tempfile replaces original only when changes are done

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Prybaby Changelog
+
+## v0.0.2 3/3/2015
+- The temp file will now only replace the original if Prybaby performed changes on it (as opposed to rewriting every file)
+- The output now declares "PRYBABY" (you know, in case you forgot what you were using) and is a little more clear.
--- lib/prybaby.rb
+++ lib/prybaby.rb
@@ -67,9 +67,11 @@ class Prybaby
       end
     end
 
-    temp_file.close
-    FileUtils.mv(temp_file.path, file)
-    self.files_modified += 1 if file_changed
+    if file_changed
+      temp_file.close
+      FileUtils.mv(temp_file.path, file)
+      self.files_modified += 1
+    end
   end
 
   def self.comment_out_breakpoints(file)
@@ -90,9 +92,11 @@ class Prybaby
       end
     end
 
-    temp_file.close
-    FileUtils.mv(temp_file.path, file)
-    self.files_modified += 1 if file_changed
+    if file_changed
+      temp_file.close
+      FileUtils.mv(temp_file.path, file)
+      self.files_modified += 1
+    end
   end
 
   def self.remove_comments_from_breakpoints(file)
@@ -111,16 +115,18 @@ class Prybaby
       end
     end
 
-    temp_file.close
-    FileUtils.mv(temp_file.path, file)
-    self.files_modified += 1 if file_changed
+    if file_changed
+      temp_file.close
+      FileUtils.mv(temp_file.path, file)
+      self.files_modified += 1
+    end
   end
 
   def self.print_stats
     if self.lines_modified == 0 && self.files_modified == 0
-      puts 'No changes were made.'
+      puts 'PRYBABY: No changes were made.'
     else
-      puts "#{self.lines_modified} lines were #{self.word} in #{self.files_modified} files."
+      puts "PRYBABY: #{self.lines_modified} lines were #{self.word} in #{self.files_modified} files."
     end
   end
 end
